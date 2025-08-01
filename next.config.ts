ff1de8306b459ffdecb82cc46d1f40d62b180abd
Muhamad Fakhri Aziz <120290082+fakhriazdev@users.noreply.github.com>
2025-08-01 23:14:02 +0700

    [FIX]: relative path hit api

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://task-manager-sms6.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
