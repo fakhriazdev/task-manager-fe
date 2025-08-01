69a9a39ec283933311d81d8d4912113964cee387
fakhriazdev <fakhriazdev@gmail.com>
2025-08-01 22:55:51 +0700

    [FEAT PROMOSI]: on progress

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'https://task-manager-rose-gamma.vercel.app/api/:path*',
-            },
-        ];
-    },
 };
 
 export default nextConfig;
