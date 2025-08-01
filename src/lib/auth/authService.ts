868d6710c03ae41908689309f4d885983965daee
fakhriazdev <fakhriazdev@gmail.com>
2025-08-02 00:11:19 +0700

    [FIX]: end point redirect loop

--- src/lib/auth/authService.ts
+++ src/lib/auth/authService.ts
@@ -2,38 +2,26 @@ import axiosInstance from "@/api/AxiosInstance";
 import {CommonResponse, LoginPayload, RegisterPayload, UserInfo} from "@/lib/auth/authTypes";
 
 
-const baseURL = '/api/auth/';
+const baseURL = '/api/auth';
 
 const AuthService = {
-    login: async (user: LoginPayload):Promise<CommonResponse<string>> => {
-        const { data } = await axiosInstance.post(`${baseURL}login`, user);
-        if (data.statusCode !== 202 && data.statusCode !== 200) {
-            throw new Error(data.message);
-        }
+    login: async (user: LoginPayload): Promise<CommonResponse<string>> => {
+        const { data } = await axiosInstance.post(`${baseURL}/login/`, user);
         return data;
     },
-    userInfo: async ():Promise<CommonResponse<UserInfo>> => {
-        const { data } = await axiosInstance.get(`${baseURL}user-info`);
-        // if (data.statusCode !== 202 && data.statusCode !== 200) {
-        //     throw new Error(data.message);
-        // }
+    register: async (user: RegisterPayload): Promise<CommonResponse<string>> => {
+        const { data } = await axiosInstance.post(`${baseURL}/register/`, user);
         return data;
     },
-    register: async (user: RegisterPayload):Promise<CommonResponse<string>> => {
-        const { data } = await axiosInstance.post(`${baseURL}register`, user);
-        if (data.statusCode !== 202 && data.statusCode !== 200) {
-            throw new Error(data.message);
-        }
+    logout: async (): Promise<CommonResponse<string>> => {
+        const { data } = await axiosInstance.post(`${baseURL}/logout/`);
         return data;
     },
-    logout: async ():Promise<CommonResponse<string>> => {
-        const { data } = await axiosInstance.post(`${baseURL}logout`);
-        if (data.statusCode !== 202 && data.statusCode !== 200) {
-            throw new Error(data.message);
-        }
+    userInfo: async (): Promise<CommonResponse<UserInfo>> => {
+        const { data } = await axiosInstance.get(`${baseURL}/user-info/`);
         return data;
     },
-
 };
+
 
 export default AuthService;
